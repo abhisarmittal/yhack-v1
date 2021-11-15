b28e0026e5483307c53955b6e3041ccca2bf0f13
yu <notyuhang@gmail.com>
2021-11-15 16:53:41 -0500

    button fix 1

--- src/puzzlePopup.js
+++ src/puzzlePopup.js
@@ -23,6 +23,7 @@ export default class PuzzlePopup extends React.Component {
 
 	handleClickOutside (event) {
 		if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+			new Promise(resolve => setTimeout(resolve, 20));
 			this.props.toggle();
 		}
 	}
